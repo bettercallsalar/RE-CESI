92ebb23393af6d57b8e8c283f5039c9a229e4425
Loic-1 <loic.vasile130@gmail.com>
2026-03-11 11:38:32 +0100

    Update Departments.cs, change attribute to required

--- src/RESR.Models/Departments/Department.cs
+++ src/RESR.Models/Departments/Department.cs
@@ -3,6 +3,6 @@ namespace RESR.Models.Departments;
 public sealed class Department
 {
     public int IdDepartment {get; set;}
-    public string Name {get; set;}
+    public required string Name {get; set;}
     public int Code {get; set;}
 }
